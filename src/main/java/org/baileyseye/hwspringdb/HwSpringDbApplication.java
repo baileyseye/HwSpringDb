e97c475caa4433c07a92e38ee3d446f62e97a954
baileyseye <79878208+baileyseye@users.noreply.github.com>
2024-03-27 20:07:53 +0300

    4

--- src/main/java/org/baileyseye/hwspringdb/HwSpringDbApplication.java
+++ src/main/java/org/baileyseye/hwspringdb/HwSpringDbApplication.java
@@ -3,6 +3,7 @@ package org.baileyseye.hwspringdb;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -10,6 +11,8 @@ public class HwSpringDbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HwSpringDbApplication.class, args);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
 	}
 
 }
--- src/main/java/org/baileyseye/hwspringdb/services/ProductManager.java
+++
@@ -1,9 +0,0 @@
-package org.baileyseye.hwspringdb.services;
-
-import org.baileyseye.hwspringdb.model.Product;
-
-import java.util.List;
-
-public interface ProductManager {
-    List<Product> findAll();
-}
